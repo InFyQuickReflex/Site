9987f4d27ad89c2445dad108e8de511bc118bddb
liya60188 <76052890+liya60188@users.noreply.github.com>
2020-12-20 12:59:00 +0100

    Create menu_utilisateur_en.php

---
+++ en/menu_utilisateur_en.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="../css/menu_utilisateur.css">
+        <title id="titre">User menu</title>
+    </head>
+
+    <body>
+        <header> 
+            <p class="logo"> <img src="../img/infy2.png" alt="logo InFy" class="img_logo"> </br> by InFy Corporation </p>
+                <nav>
+                    <ul id="menu">
+                        <li><a href="menu_principal_en.php">Home</a></li>
+                        <li><a href="notre_produit_en.php">Our Product</a></li>
+                    </ul>
+                </nav>
+
+                <figure>
+                    <img src="logoprofil2.png" alt = "profile logo" id="tof">
+                    <figcaption id="monprofil"><a href="" id="monprofil">My profile</a></figcaption>
+                    <a href="">Log out</a>
+                </figure>
+
+                <nav>
+                <ul id="langue">
+                    <li><a href="../fr/menu_utilisateur_fr.php"><img src="../img/ic_fr.png" class="icone"></a></li>
+                    <li><a href=""><img src="../img/ic_en.png" class="icone"></a></li>
+                </ul>
+            </nav>
+        </header>
+
+        <div id="conteneur" class="conteneur">
+            <div id="nouveautest" class="button">
+                <a href="" ><p>Start a new test</p></a>
+            </div>
+
+            <div id="nouveautest" class="button">
+                <a href="test_resume_en.php" ><p>Acces to my data</p></a>
+            </div>
+
+            <div id="nouveautest" class="button" >
+                <a href="" ><p>Acces to the FAQ</p></a>
+            </div>
+        </div>
+
+        <footer>
+            <p class = "CGU"> Legal mentions:  </p>
+            <nav>
+                <ul id="footer">
+                    <li> <a href=''> Contact us </a> </li>
+                    <li> <a href=''> FAQ </a> </li>
+                </ul>
+            </nav>
+        </footer>
+    </body>
+</html>

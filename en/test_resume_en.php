03fa4c34ad2c5f21a42043eaacf30cee10116c89
liya60188 <76052890+liya60188@users.noreply.github.com>
2020-12-20 12:58:28 +0100

    Create test_resume_en.php

---
+++ en/test_resume_en.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="../css/test_resume.css">
+        <title id="titre">Summary of tests</title>    
+    </head>
+
+    <body>
+        <header> 
+            <p class="logo"> <img src="../img/infy2.png" alt="logo InFy" class="img_logo"> </br> by InFy Corporation </p>
+
+            <nav>
+                <ul id="menu">
+                    <li><a href="menu_principal_en.php">Home</a></li>
+                    <li><a href="notre_produit_en.php">Our Product</a></li>
+                    <li><a href="profil_utilisateur_en.php" id="monprofil">My Profile</a></li>
+                </ul>
+            </nav>
+
+            <nav>
+                <ul id="langue">
+                    <li><a href="..//fr/menu_principal_fr.php"><img src="../img/ic_fr.png" class="icone"></a></li>
+                    <li><a href=""><img src="../img/ic_en.png" class="icone"></a></li>
+                </ul>
+            </nav> 
+        </header>
+
+        <div id="test" class="button">
+            <p>Test 1: result</p>	
+        </div>
+
+        <div id="test" class="button">
+            <p>Test 3: result</p>		
+        </div>
+
+        <div id="test" class="button">
+            <p>Test 3: result</p>		
+        </div>
+
+        <footer>
+            <p class = "CGU"> Legal mentions  </p>
+            <nav>
+                <ul id="footer">
+                    <li> <a href='#'> Contact us </a> </li>
+                    <li> <a href='#'> FAQ </a> </li>
+                </ul>
+            </nav>
+        </footer>
+    </body>
+</html>

45439ef6e3f7015a9091c0ea6fdc8e8f31fbf455
liya60188 <76052890+liya60188@users.noreply.github.com>
2020-12-20 12:57:37 +0100

    Create profil_utilisateur_en.php

---
+++ en/profil_utilisateur_en.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="../css/profil_utilisateur.css">
+        <title id="titre">User profile</title>
+    </head>
+
+    <body>
+        <header> 
+            <p class="logo"> <img src="../img/infy2.png" alt="logo InFy" class="img_logo"> </br> by InFy Corporation </p>
+            <nav>
+                <ul id="menu">
+                    <li><a href="menu_principal_en.php">Home</a></li>
+                    <li><a href="notre_produit_en.php">Our Product</a></li>
+                    <li><a href="menu_utilisateur_en.php"">User menu</a></li>
+                </ul>
+            </nav>
+
+            <nav>
+                <ul id="langue">
+                    <li><a href="../fr/profil_utilisateur_fr.php"><img src="../img/ic_fr.png" class="icone"></a></li>
+                    <li><a href=""><img src="../img/ic_en.png" class="icone"></a></li>
+                </ul>
+            </nav>
+        </header>
+
+        <h1>Welcome "nom et prénom de la personne"</h1>
+        
+        <div id="identité">
+            <p>
+                Last name:  ###PHP###
+                </br>
+                </br>
+                First name:  ###PHP###
+                </br>
+                </br>
+                Age:   ###PHP###
+                </br>
+                </br>
+                Email:   ###PHP###
+            </p>
+        </div>
+
+        <footer>
+            <p class = "CGU"> Legal mentions: </p>
+            <nav>
+                <ul id="footer">
+                    <li> <a href='#'> Contact us </a> </li>
+                    <li> <a href='#'> FAQ </a> </li>
+                </ul>
+            </nav>
+        </footer>
+    </body>
+</html>

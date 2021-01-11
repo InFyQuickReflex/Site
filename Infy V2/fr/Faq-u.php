f2c0f0f35022387795595b048b803f05819db50d
Jeremy1999de <77159194+Jeremy1999de@users.noreply.github.com>
2021-01-11 17:39:22 +0100

    Add files via upload

---
+++ Infy V2/fr/Faq-u.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>FAQ - Utilisateur</title>
+        <link rel="stylesheet" href="../css/header_footer.css">
+        <link rel="stylesheet" href="../css/style.css">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+        <link rel="stylesheet" href="stylefaq.css">
+    </head>
+
+    <body>
+      <?php include("header_footer/header.php")?>
+
+      <main>
+
+            <h1>Consultez la FAQ !</h1>
+
+      <button class="accordion">Comment se connecter à son boitier ?</button>
+        <div class="panel">
+          <p>Réponse 1. ..............................................................................</p>
+        </div>
+
+      <button class="accordion">Comment voir ses résulats ?</button>
+        <div class="panel">
+          <p>Réponse deux..............................................................</p>
+        </div>
+
+      <button class="accordion">Comment refaire un test ?</button>
+        <div class="panel">
+          <p>Réponse 3.............................................................................</p>
+        </div>
+    
+      </main>
+
+      <script src="faq.js"></script>
+      <?php include("header_footer/footer.php")?>
+    </body>
+</html>

e95861ba574c687a81116c664eeb16211953c9e1
liya60188 <lina.yanouri@gmail.com>
2021-01-11 13:02:34 +0100

    Chagnegement de la bdd

--- Infy V2/php_fr/connexion.php
+++ Infy V2/php_fr/connexion.php
@@ -3,8 +3,9 @@ session_start();
 
 try
 {
-	$bdd = new PDO('mysql:host=localhost;dbname=infy;charset=utf8', 'root', 'root');
+	 $bdd = new PDO("mysql:host=mysql-g5c.alwaysdata.net;dbname=g5c_infy;charset=utf8", "g5c", "informatique");
 }
+
 catch (Exception $e)
 {
         die('Erreur : ' . $e->getMessage());
--- Infy V2/php_fr/creer.php
+++ Infy V2/php_fr/creer.php
@@ -1,12 +1,12 @@
 <?php
 try
 {
-    $bdd = new PDO("mysql:host=localhost;dbname=infy;charset=utf8", "root", "root");
+    $bdd = new PDO("mysql:host=mysql-g5c.alwaysdata.net;dbname=g5c_infy;charset=utf8", "g5c", "informatique");
 }
 
 catch (Exception $e)
 {
-    die("Erreur : " . $e->getMessage());
+    die("Erreur : " . $e->getMessage()); 
 }
 
 $prenom= htmlspecialchars($_POST["prenom"]);
@@ -14,9 +14,11 @@ $nom= htmlspecialchars($_POST["nom"]);
 $identifiant= htmlspecialchars($_POST["identifiant"]);
 $email= htmlspecialchars($_POST["email"]);
 $mot_de_passe= password_hash(htmlspecialchars($_POST["mot_de_passe"]), PASSWORD_DEFAULT);
+$date_de_naissance= htmlspecialchars($_POST["date_de_naissance"]);
+$telephone= htmlspecialchars($_POST["telephone"]);
 
-$req = $bdd->prepare("INSERT INTO users(prenom, nom, identifiant, email, mot_de_passe, permission) VALUES(?, ?, ?, ?, ?, 'utilisateur')");
-$req->execute(array($prenom, $nom, $identifiant, $email, $mot_de_passe));
+$req = $bdd->prepare("INSERT INTO users(prenom, nom, identifiant, email, mot_de_passe, permission, date_de_naissance, telephone) VALUES(?, ?, ?, ?, ?, 'utilisateur', ?, ?)");
+$req->execute(array($prenom, $nom, $identifiant, $email, $mot_de_passe, $date_de_naissance, $telephone));
 
 $req->closeCursor();
 

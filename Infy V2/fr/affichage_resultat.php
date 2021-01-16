b6cb65f94c647043b57ac712196dc291bea4c38c
liya60188 <lina.yanouri@gmail.com>
2021-01-16 17:02:30 +0100

    Update affichage_resultat.php

--- Infy V2/fr/affichage_resultat.php
+++ Infy V2/fr/affichage_resultat.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <title>Titre</title>
+        <title>Vos résultats</title>
         <link rel="stylesheet" href="../css/header_footer.css">
         <link rel="stylesheet" href="../css/style.css">
         <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">

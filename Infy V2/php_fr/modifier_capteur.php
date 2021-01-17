73cf6c35ea9be6e8bb80770bb84a15f0ae20ca7b
liya60188 <lina.yanouri@gmail.com>
2021-01-17 22:57:55 +0100

    Update
    liens vers fichier "fonctions"

--- Infy V2/php_fr/modifier_capteur.php
+++ Infy V2/php_fr/modifier_capteur.php
@@ -3,7 +3,7 @@ session_start();
 if(isset($_SESSION["ID"]))
         {
 			include('connexionbdd.php');
-			include('fonctions_gerer_capteurs.php');
+			include('fonctions/fonctions_gerer_capteurs.php');
 			$permission = permissionUser($bdd,$_SESSION["ID"]);
 			if($permission == "administrateur")
 			{

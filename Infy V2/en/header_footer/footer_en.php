89a60d77d977fc133d185942f63516ed08af37f9
liya60188 <lina.yanouri@gmail.com>
2021-01-18 19:55:06 +0100

    Déplacement dans un dossier

---
+++ Infy V2/en/header_footer/footer_en.php
@@ -0,0 +1,10 @@
+<footer>
+    <a href="CGU.php">Legal notice </a>
+
+    <nav>
+        <ul>
+            <li> <a href="nous_contacter.php"> Contact us </a> </li>
+            <li> <a href="Faq-u.php"> FAQ </a> </li>
+        </ul>
+    </nav>
+</footer>

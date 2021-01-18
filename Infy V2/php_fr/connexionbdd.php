19185a1769d11b1cf265bd049a031742a224e397
liya60188 <lina.yanouri@gmail.com>
2021-01-18 19:51:21 +0100

    Update connexionbdd.php
    encore

--- Infy V2/php_fr/connexionbdd.php
+++ Infy V2/php_fr/connexionbdd.php
@@ -1,7 +1,7 @@
 <?php
 try
 {
-	$bdd = new PDO("mysql:host= mysql-infy.alwaysdata.net;dbname=infy_QR;charset=utf8", "infy", "g5cappbdd");
+	$bdd = new PDO("mysql:host=mysql-infy.alwaysdata.net;dbname=infy_qr;charset=utf8", "infy", "g5cappbdd");
 }
 catch (Exception $e)
 {

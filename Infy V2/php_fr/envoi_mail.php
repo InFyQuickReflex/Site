32f7fe788bb642caf02d0012149ec9128090c863
ThomasRaboteau <77160825+ThomasRaboteau@users.noreply.github.com>
2021-01-18 20:18:31 +0100

    update envoi_mail.php
    - Changement de l'adresse mail utilisé
    
    - Correction de la connexion à la BDD

--- Infy V2/php_fr/envoi_mail.php
+++ Infy V2/php_fr/envoi_mail.php
@@ -1,4 +1,4 @@
-<?php/*
+<?php
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
@@ -6,23 +6,23 @@ require '../phpmailer/src/Exception.php';
 require '../phpmailer/src/PHPMailer.php';
 require '../phpmailer/src/SMTP.php';
 $mail = new PHPMailer(); 
+echo (extension_loaded('openssl')?'SSL loaded':'SSL not loaded')."<br>";
 $mail->IsSMTP();
 $mail->SMTPAuth = true;
-$mail->SMTPOptions = array('ssl' => array('verify_peer' => false,'verify_peer_name' => false,'allow_self_signed' => true)); 
-// ignorer l'erreur de certificat.
-//$mail->Host       = "smtp-g5c.alwaysdata.net";  
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+$mail->Host       = "mail.gmx.com";  
 $mail->Port       = 587;
-//$mail->Username   = "g5c@alwaysdata.net";
-//$mail->Password   = "informatique";        
-$mail->From       = htmlspecialchars($_POST['replyto']);
+$mail->Username   = "infy@gmx.fr";
+$mail->Password   = "QR-G5C-2021";        
+$mail->From       = htmlspecialchars('infy@gmx.fr');
 $mail->FromName   = htmlspecialchars($_POST['prenom'].' '.$_POST['nom']);
 $mail->Subject    = htmlspecialchars($_POST['sujet']);
 $mail->AltBody    = htmlspecialchars($_POST['message']); //Body au format texte
 $mail->WordWrap   = 50; // nombre de caractères pour le retour à la ligne automatique
 $mail->AddReplyTo(htmlspecialchars($_POST['replyto']),htmlspecialchars($_POST['prenom'].' '.$_POST['nom']));
-//$mail->AddAddress("g5c@alwaysdata.net");
+$mail->AddAddress("infy@gmx.fr");
 $mail->MsgHTML(htmlspecialchars($_POST['message']));
-$mail->IsHTML(false); 	
+$mail->IsHTML(true); 	
 if(!$mail->Send()) {
 echo "Mailer Error: " . $mail->ErrorInfo;
 } else {

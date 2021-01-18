97902a1c1afcfe2524f7ca8e13bcd01fcb873fff
liya60188 <lina.yanouri@gmail.com>
2021-01-18 19:33:40 +0100

    Changement page FAQ + Supprimer "gerer les droits d'acces"

--- Infy V2/fr/Faq.php
+++ Infy V2/fr/Faq.php
@@ -10,27 +10,24 @@
     </head> 
 
     <body>
-      <?php include("header_footer/header.php")
+      <?php include("header_footer/header.php");
       include('../php_fr/connexionbdd.php');?>
 
       <main>
-      <h1>Consultez la FAQ !</h1>
+        <br><br>
+        <h1>Consultez la FAQ !</h1>
         <?php
         $req = $bdd->query('SELECT * FROM FAQ');
-        $donnees = $req->fetch();
-        foreach ($faq as $el){
-          $id= $el['id_FAQ'];
-          $reponse = $el['reponse'];
-          $question = $el['question'];
-        }
-        ?>
 
-        <div class=<?=$question?>>    
-        <button class="accordion" onClick="BasculerAffichage('<?=$rep?>');">
-          <h3><?=$key['Question']?></h3>
-        </button>
-        </div>
-
+        while($donnees = $req->fetch())
+          {?>
+            <button class="accordion">
+              <h3><?php echo $donnees['question_fr']?></h3>
+              </button>
+            <div class="panel">    
+              <?php echo $donnees['reponse_fr']?>
+            </div>
+        <?php }?>
 
       <script src="../css/faq.js"></script>
       <?php include("header_footer/footer.php")?>

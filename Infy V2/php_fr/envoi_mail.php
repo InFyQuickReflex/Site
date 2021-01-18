19a76f555a8f268a75855501b9f1416373f88335
liya60188 <lina.yanouri@gmail.com>
2021-01-18 15:30:03 +0100

    Update envoi_mail.php
    En attendant de changer l'adresse mail

--- Infy V2/php_fr/envoi_mail.php
+++ Infy V2/php_fr/envoi_mail.php
@@ -10,17 +10,17 @@ $mail->IsSMTP();
 $mail->SMTPAuth = true;
 $mail->SMTPOptions = array('ssl' => array('verify_peer' => false,'verify_peer_name' => false,'allow_self_signed' => true)); 
 // ignorer l'erreur de certificat.
-$mail->Host       = "smtp-g5c.alwaysdata.net";  
+//$mail->Host       = "smtp-g5c.alwaysdata.net";  
 $mail->Port       = 587;
-$mail->Username   = "g5c@alwaysdata.net";
-$mail->Password   = "informatique";        
+//$mail->Username   = "g5c@alwaysdata.net";
+//$mail->Password   = "informatique";        
 $mail->From       = htmlspecialchars($_POST['replyto']);
 $mail->FromName   = htmlspecialchars($_POST['prenom'].' '.$_POST['nom']);
 $mail->Subject    = htmlspecialchars($_POST['sujet']);
 $mail->AltBody    = htmlspecialchars($_POST['message']); //Body au format texte
 $mail->WordWrap   = 50; // nombre de caractères pour le retour à la ligne automatique
 $mail->AddReplyTo(htmlspecialchars($_POST['replyto']),htmlspecialchars($_POST['prenom'].' '.$_POST['nom']));
-$mail->AddAddress("g5c@alwaysdata.net");
+//$mail->AddAddress("g5c@alwaysdata.net");
 $mail->MsgHTML(htmlspecialchars($_POST['message']));
 $mail->IsHTML(false); 	
 if(!$mail->Send()) {

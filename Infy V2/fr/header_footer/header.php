decf1a582b3146c4c718e3a95fdd806d19d408b7
liya60188 <lina.yanouri@gmail.com>
2021-01-19 14:55:17 +0100

    footer

--- Infy V2/fr/header_footer/header.php
+++ Infy V2/fr/header_footer/header.php
@@ -46,15 +46,16 @@ session_start();
             {
                 ?>
                 <li><a href="page_connexion.php">SE CONNECTER</a></li>
-                <nav class="langue">
-                    <ul>
-                        <li><a href=""><img src="../img/ic_fr.png" class="icone"></a></li>
-                        <li><a href="../en/menu_principal_en.php"><img src="../img/ic_en.png" class="icone"></a></li>
-                    </ul>
-    </nav>
+                
                 <?php
             }
             ?>
+            <nav class="langue">
+                <ul>
+                    <li><a href=""><img src="../img/ic_fr.png" class="icone"></a></li>
+                    <li><a href="../en/menu_principal_en.php"><img src="../img/ic_en.png" class="icone"></a></li>
+                </ul>
+            </nav>
         </ul>
     </nav>
 

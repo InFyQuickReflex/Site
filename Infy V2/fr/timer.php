4ecb7352c84290753fd414118c54011a2e9b386a
Maleau <74731212+Maleau@users.noreply.github.com>
2021-01-19 15:06:52 +0100

    Add files via upload

---
+++ Infy V2/fr/timer.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Titre</title>
+        <link rel="stylesheet" href="../css/header_footer.css">
+        <link rel="stylesheet" href="../css/commencer_test.css">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    </head>
+
+    <body>
+      <?php include("header_footer/header.php")?>
+
+      <main>
+        <script>
+            var sec = 5;
+            function tick()
+            {
+                document.getElementById('decompte').innerText = sec;
+        
+                if(sec == 0)
+                {
+                    document.getElementById('decompte').innerText = 'Début du test';
+                    document.getElementById('cache').style.display = 'block';
+                    window.clearInterval(timer);
+                }
+
+                sec--;
+            }
+            var timer = window.setInterval(tick, 1000);
+        </script>
+
+        <div class="timer">
+            <p id="decompte">Le test va bientot commencer</p>
+            <div id="cache" style="display: none;">  
+            </div>
+        </div>
+      </main>
+
+      <?php include("header_footer/footer.php")?>
+    </body>
+</html>

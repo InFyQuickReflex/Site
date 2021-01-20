fc8eb8ee39a666bb6a1e25527419006daa9c1aaa
Marguerite25 <76174786+Marguerite25@users.noreply.github.com>
2021-01-20 10:46:52 +0100

    Add files via upload

---
+++ Infy V2/en/profil_utilisateur_en.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"> 
+        <title>Infy - My profile</title>
+        <link rel="stylesheet" href="../css/header_footer.css">
+        <link rel="stylesheet" href="../css/profil_utilisateur.css">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    </head>
+
+    <body>
+      <?php include("header_footer/header_en.php")?>
+
+      <main>
+          <?php
+          if(isset($_SESSION["ID"]))
+          {
+            ?>
+            <br>
+            <h2>Users profile</h2>
+
+            <div class="profil">
+                <img src="../img/pp.png">
+                <p>
+                    <?php
+                    include('../php_fr/connexionbdd.php');
+    
+                    $req = $bdd->prepare("SELECT prenom, nom, identifiant, email, permission FROM users WHERE id_user = ?");
+                    $req->execute(array($_SESSION["ID"]));
+                    $donnees = $req->fetch();
+
+                    if($donnees["permission"] != "utilisateur")
+                    {
+                        if($donnees["permission"] == "administrateur")
+                        {
+                            header("Location: profil_administrateur.php");
+                        }
+
+                        else if($donnees["permission"] == "gestionnaire")
+                        {
+                            header("Location: profil_gestionnaire.php");
+                        }
+                    }
+
+                    else
+                    {
+                        echo "<strong>First name</strong> : ".$donnees["prenom"]."<br>";
+                        echo "<strong>Last name</strong> : ".$donnees["nom"]."<br>";
+                        echo "<strong>Username</strong> : ".$donnees["identifiant"]."<br>";
+                        echo "<strong>Email</strong> : ".$donnees["email"]."<br><br>";
+                    }
+                    
+                    $req->closeCursor();
+                    ?>
+    
+                    <a href="../php_en/connexion_en.php" class="button">Log out</a>
+                </p>
+            </div>
+
+            <br>
+            <div class="start_test">
+                <a href="commencer_test_en.php">Start a new test</a>
+            </div>
+            
+            <br>
+            <h3>Test history</h3>
+            
+            <div class="test">
+               <?php
+               $reponse = $bdd->prepare("SELECT users.id_user, id_test, date_test FROM tests INNER JOIN users ON (tests.id_user = users.id_user AND users.id_user = ?) ORDER BY date_test DESC LIMIT 0,4");
+                $reponse->execute(array($_SESSION["ID"])); 
+
+                while ($donnees = $reponse->fetch())
+                {
+                    echo '<p>Test du ' .$donnees["date_test"]. ' : <a href="affichage_resultat.php?ID='.$donnees["id_user"].'&IDtest='.$donnees["id_test"].'"> Results </a> </br></p>';       
+                }
+               ?>
+            </div>
+                       
+            <?php
+          }
+
+          else
+          {
+              header("Location: menu_principal_en.php");
+          }
+          ?>  
+      </main>
+
+      <?php include("header_footer/footer_en.php")?>
+    </body>
+</html>
+

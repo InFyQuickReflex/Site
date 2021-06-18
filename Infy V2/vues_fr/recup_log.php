1f039b6782e6cfe3e3660e0e3a92889b9f54e62c
liya60188 <lina.yanouri@gmail.com>
2021-06-18 14:02:08 +0200

    Update recup_log.php

--- Infy V2/vues_fr/recup_log.php
+++ Infy V2/vues_fr/recup_log.php
@@ -1,26 +1,33 @@
 <?php
+include('../controleur_fr/connexionbdd.php');
+
 $ch = curl_init();
 $data = file_get_contents('http://projets-tomcat.isep.fr:8080/appService?ACTION=GETLOG&TEAM=G5CA'); 
-echo "Raw Data:<br />";
-echo("$data");
+//echo "Raw Data:<br />";
+//echo("$data");
 
 $data_tab = str_split($data,33);
-echo "Tabular Data:<br />";
-for($i=0, $size=count($data_tab); $i<$size; $i++){
-//echo "Trame $i: $data_tab[$i]<br />";
-$trame = $data_tab[$i];
+//echo "Tabular Data:<br />";
 
-list($t, $o, $r, $c, $n, $v, $a, $x, $year, $month, $day, $hour, $min, $sec) =
-sscanf($trame,"%1s%4s%1s%1s%2s%4s%4s%2s%4s%2s%2s%2s%2s%2s");
-$vhexa = hexdec($v);
-echo("<br />$t,$o,$r,$c,$n,$vhexa,$a,$x,$year,$month,$day,$hour,$min,$sec<br />");
+$size=count($data_tab);
+for($i=0;  $i<$size; $i++){
+echo "Trame $i: $data_tab[$i]<br />";
 }
-/*
-$trame = $data_tab[1];
+
+echo "Taille: $size";
+$trame = $data_tab[$size-2];
 
 list($t, $o, $r, $c, $n, $v, $a, $x, $year, $month, $day, $hour, $min, $sec) =
 sscanf($trame,"%1s%4s%1s%1s%2s%4s%4s%2s%4s%2s%2s%2s%2s%2s");
-$vhexa = hexdec($v);
-echo("<br />$t,$o,$r,$c,$n,$vhexa,$a,$x,$year,$month,$day,$hour,$min,$sec<br />");
-*/
+$vConverti = $v/100;
+$date = "$year/$month/$day";
+$datehoraire = "$hour:$min:$sec"; 
+echo("<br />$t,$o,$r,$c,$n,$vConverti,$a,$x,$date,$datehoraire<br />");
+
+
+$req = $bdd->prepare("INSERT INTO mesures(date_mesure,valeur, unite_mesure, id_capteurs, id_type_mesures) VALUES(?,?,?,?,?)");
+$req->execute(array($datehoraire, $vConverti, "degré", $c,"1"));
+
+$req2 = $bdd->prepare("INSERT INTO tests(date_test) VALUES(?)");
+$req2->execute(array($date));
 ?>
